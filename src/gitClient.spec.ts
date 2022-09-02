1a6edc32e96d9023727ae5afca976d275608e2b2
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-09-03 00:02:58 +0200

    Initial commit

---
+++ src/gitClient.spec.ts
@@ -0,0 +1,29 @@
+import { describe, it, vi, expect } from 'vitest'
+
+import GitClient from './gitClient'
+import repositoriesResponse from './repositories-response.json'
+import repositoriesExtracted from './repositories-extracted.json'
+
+describe('gitClient', () => {
+    vi.mock('@octokit/rest', () => ({
+        Octokit: vi.fn().mockImplementation(() => ({
+            rest: {
+                repos: {
+                    listForUser: () => Promise.resolve({ data: repositoriesResponse })
+                },
+            }
+        }))
+    }))
+
+    const gitClient = new GitClient()
+
+    describe('retrieveRepositories', () => {
+        it('should retrieve all repositories', async () => {
+            const repositories = await gitClient.retrieveRepositories()
+
+            expect(repositories).toHaveLength(10)
+
+            expect(repositories).toEqual(repositoriesExtracted)
+        })
+    })
+})
---
+++ src/gitClient.ts
@@ -0,0 +1,31 @@
+import { Octokit, RestEndpointMethodTypes } from '@octokit/rest'
+
+interface GitTemplateRepositories {
+    downloadUrl: string
+    name: string
+}
+
+const ORGANIZATION_NAME = 'orchy-mfe'
+
+type UserRepositoriesResponse = RestEndpointMethodTypes["repos"]["listForUser"]["response"]["data"][0]
+
+const isTemplateRepository = (repository: UserRepositoriesResponse) => repository.name.startsWith('orchy') && repository.name.endsWith('template')
+
+const createZipballUrl = (repository: UserRepositoriesResponse) => `${repository.url}/zipball`
+
+export default class GitClient {
+    private octokit: Octokit = new Octokit()
+
+    async retrieveRepositories(): Promise<GitTemplateRepositories[]> {
+        const repositoriesList = await this.octokit.rest.repos.listForUser({ username: ORGANIZATION_NAME })
+
+        return repositoriesList.data
+            .filter(isTemplateRepository)
+            .map(repository => ({
+                name: repository.name,
+                downloadUrl: createZipballUrl(repository)
+            }))
+    }
+
+
+}
