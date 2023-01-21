e034c361431b0046f1f5ba671784f375c5c95d8e
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-01-21 11:37:26 +0100

    fix: clean build config

--- build.config.ts
+++ build.config.ts
@@ -1,8 +1,4 @@
-import path from 'node:path'
-import url from 'node:url'
-import { defineBuildConfig } from 'unbuild'
-
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+import {defineBuildConfig} from 'unbuild'
 
 export default defineBuildConfig({
   entries: ['src/index'],
